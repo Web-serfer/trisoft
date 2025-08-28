3ffe9e98210b238bb5e0e36dc6a501cd9a5c44f3
RediBedi <redibedi2019@gmail.com>
2025-08-29 01:45:46 +0500

    28/08/25

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,24 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
-import preact from '@astrojs/preact'
 import icon from 'astro-icon'
 import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'http://trisoft.ru',
-	integrations: [preact(), icon()],
+	integrations: [icon()],
 	vite: {
-		plugins: [tailwindcss()],
-		resolve: {
-			alias: {
-				react: 'preact/compat',
-				'react-dom/test-utils': 'preact/test-utils',
-				'react-dom': 'preact/compat',
-				'react/jsx-runtime': 'preact/jsx-runtime',
-			},
-		},
+		plugins: [tailwindcss()]
 	},
 	output: 'server',
 	adapter: vercel(),
