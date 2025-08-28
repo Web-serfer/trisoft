e55ab37e82f06b4d6219626199ec32674fb753fa
RediBedi <redibedi2019@gmail.com>
2025-08-29 00:29:35 +0500

    28/08/25

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,25 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import preact from '@astrojs/preact';
-import icon from "astro-icon";
-import vercel from "@astrojs/vercel";
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import preact from '@astrojs/preact'
+import icon from 'astro-icon'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://trisoft.ru",
-  integrations: [preact(),icon()],
-  vite: {
-    plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        "react": "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-        "react/jsx-runtime": "preact/jsx-runtime"
-      },
-    },
-  },
-  output: "server",
-  adapter: vercel(),
-});
+	site: 'http://trisoft.ru',
+	integrations: [preact(), icon()],
+	vite: {
+		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				react: 'preact/compat',
+				'react-dom/test-utils': 'preact/test-utils',
+				'react-dom': 'preact/compat',
+				'react/jsx-runtime': 'preact/jsx-runtime',
+			},
+		},
+	},
+	output: 'server',
+	adapter: vercel(),
+})
--- frontend/src/components/layout/header/navData.ts
+++ frontend/src/components/layout/header/navData.ts
@@ -1,32 +1,32 @@
 export const menus = [
-  {
-    name: "Главная",
-    url: "/",
-  },
-  {
-    name: "Услуги",
-    url: "/services",
-    children: [
-      { name: "Разработка 1С", url: "/services/development" },
-      { name: "Внедрение", url: "/services/implementation" },
-      { name: "Сопровождение", url: "/services/support" },
-      { name: "Аутсорс/аутстафф", url: "/services/outsource" },
-    ],
-  },
-  {
-    name: "Блог",
-    url: "/blog",
-  },
-  {
-    name: "Цены",
-    url: "/prices",
-  },
-  {
-    name: "О Компании",
-    url: "/about",
-  },
-  {
-    name: "Контакты",
-    url: "/contacts",
-  },
-];
+	{
+		name: 'Главная',
+		url: '/',
+	},
+	{
+		name: 'Услуги',
+		url: '/services',
+		children: [
+			{ name: 'Разработка 1С', url: '/services/development' },
+			{ name: 'Внедрение', url: '/services/implementation' },
+			{ name: 'Сопровождение', url: '/services/support' },
+			{ name: 'Аутсорс/аутстафф', url: '/services/outsource' },
+		],
+	},
+	{
+		name: 'Блог',
+		url: '/blog',
+	},
+	{
+		name: 'Цены',
+		url: '/prices',
+	},
+	{
+		name: 'О Компании',
+		url: '/about',
+	},
+	{
+		name: 'Контакты',
+		url: '/contacts',
+	},
+]
