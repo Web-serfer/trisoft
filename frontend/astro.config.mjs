21435cb737e1454bf51dd9027cb7340e3cb23e00
RediBedi <redibedi2019@gmail.com>
2025-09-10 19:02:04 +0500

    10/09/25

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
+import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
-import icon from 'astro-icon'
-import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel';
+
+
+import solidJs from '@astrojs/solid-js';
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'http://trisoft.ru',
-	integrations: [icon()],
-	vite: {
-		plugins: [tailwindcss()]
-	},
-	output: 'server',
-	adapter: vercel(),
+  site: 'https://trisoft.ru',
+  integrations: [icon(), solidJs()],
+  vite: {
+  	plugins: [tailwindcss()]
+  },
+  adapter: vercel(),
+  output: 'server'
 })
---
+++ frontend/src/env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="astro/client" />
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.webp';
+declare module '*.avif';
